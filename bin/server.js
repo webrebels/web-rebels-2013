0df16ffd27da5bdb3648af22d4d5eb8e2333877c
Espen Dalløkken <leftie.friele@gmail.com>
2013-02-14 20:39:26 +0100

    adding open mic night page and more sponsoring content

--- bin/server.js
+++ bin/server.js
@@ -24,10 +24,13 @@ app.set('view engine', 'ejs');
 
 // Start http server
 app.get('/', function(req, res){
-	res.render('frontpage', { pageTitle: 'Web Rebels Conference 2013' });
+	res.render('frontpage', { pageTitle: 'Web Rebels ☠ Oslo ☠ 2013' });
 });
 app.get('/sponsors', function(req,res){
-	res.render('sponsors', {pageTitle: 'Sponsoring options for the Web Rebels Conference 2013'})
+	res.render('sponsors', {pageTitle: 'Sponsoring options for the Web Rebels ☠ Oslo 2013'})
+});
+app.get('/openmic', function(req,res){
+	res.render('openmic', {pageTitle: 'Open Mic Night - Web Rebels ☠ Oslo 2013'})
 });
 httpServer.listen(port);
 console.info('WR2013 is running at http://localhost:' + port + '/');

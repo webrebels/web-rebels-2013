0896f641ac8127cb8b1eb553c16cca2343190a9c
Trygve Lie <github@trygve-lie.com>
2013-02-28 13:13:52 +0100

    Added some DOM functions for future use

--- public/js/streams.js
+++ public/js/streams.js
@@ -1,3 +1,30 @@
+function hasWebSockets(){
+    return 'WebSocket' in window && window.WebSocket.CLOSING === 2;
+}
+
+function addFirstListElement(root, element) {
+    var first   = root.firstChild,
+        el      = document.createElement('li');
+    el.appendChild(element);
+    root.insertBefore(el, first);
+}
+
+function removeLastListElement(root) {
+    var last            = root.lastChild,
+        parentElement   = last.parentNode;
+
+    parentElement.removeChild(last);
+}
+
+function setIrcUpdate(message) {
+    console.log('update', message);
+}
+
+function setIrcLog(messages) {
+    console.log('log', messages);
+}
+
+
 var socketInit = function() {
     var socket = new WebSocket('ws://' + window.location.host + '/stream');
 
@@ -6,8 +33,15 @@ var socketInit = function() {
     });
 
     socket.addEventListener('message', function(msg) {
-        var obj = JSON.parse(msg.data);
-        console.log(obj);
+        var obj = JSON.parse(msg);
+
+        if (obj.type === 'irc:log') {
+            setIrcLog(msg);
+
+        } else if (obj.type === 'irc:update') {
+            setIrcUpdate(msg);
+
+        }
     });
 
     socket.addEventListener('close', function(msg) {
@@ -15,4 +49,6 @@ var socketInit = function() {
     });
 
 }
-socketInit();
+if (hasWebSockets()) {
+    socketInit();
+}
